d262274e24b6bf205c4a8ad42ed079611fbcf2b3
Diego Sevilla <sevilla.diego8@gmail.com>
2021-03-07 13:55:57 -0600

    feat: :sparkles: estados de afn para tabla de transiciones del afd listo

--- AFNtoAFD/afn_to_afd.py
+++ AFNtoAFD/afn_to_afd.py
@@ -22,10 +22,9 @@ THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
 
 
 # Definitions _________________________
-conjuntos = []  # contendra los estados
-afn_tran_table = [] #tabla de transisicion del automata finito no determinista
-
-afd_tran_table= []
+#conjuntos = []  # contendra los estados
+#afn_tran_table = [] #tabla de transisicion del automata finito no determinista
+#afd_tran_table= []
 
 # Functions area______________________________________________
 def transition_afn_table(csvfile='afn-tran-table.csv'):
@@ -76,7 +75,7 @@ def move(statesSet=[],transition=None,afnTranTable=[]):
      - return - [3,8]
     '''
     if(transition == None):
-        return '0'
+        return ['0']
     else:
         result = []
         transition_column = afnTranTable[transition+1]
@@ -84,9 +83,9 @@ def move(statesSet=[],transition=None,afnTranTable=[]):
             state_of_Set_param = statesSet[i] #0
             state = transition_column[int(state_of_Set_param)]
             # Se revisa que el estado no sea nulo
-            if(state != 'vacio' and len(state) >1):
+            if(state != 'vacio' and int(state) == False):
                 result.append(transition_column[int(state_of_Set_param)].split(','))
-            elif(state != 'vacio' and len(state) == 1):
+            elif(state != 'vacio' and int(state)):
                 result.append(transition_column[int(state_of_Set_param)])
 
         return result
@@ -110,7 +109,6 @@ def find_epsilon_transitions_state(epsilonStates,afnStates,state):
 
     return statesThroughEpsilon
 
-
 def epsilon_closure(move,afnTranTable):
     '''
     Funcion de cerradura epsilon que retorna todos los estados a los que se 
@@ -123,16 +121,15 @@ def epsilon_closure(move,afnTranTable):
      - return - Dtran(A,a) = epsilon_closure(move(A,a)) = [1,2,3,4,6,7,8]
     '''
     stateSet = []
-    if (move[0] == '0'):
-        stateSet.append(move[0])
-        epsilonStates = afnTranTable[len(afnTranTable)-1] #ultima posicion de afnTranTable = Estados_transicion_epsilon
+    epsilonStates = afnTranTable[len(afnTranTable)-1] #ultima posicion de afnTranTable = Estados_transicion_epsilon
+    
+    for m in range(len(move)):
+        i = int(move[m])
+        stateSet.append(move[m])
 
         # epsilon = [['1,7'],['2,4'],...,[s]]
-        #for i in range(len(epsilonStates)):
-
-        i = 0
+        
         # Ej. epsilonState = '1,7'
-        print(epsilonStates[i].split(','))
         if(len(epsilonStates[i].split(',')) > 1 and epsilonStates[i] != 'vacio'): 
             epsilonStateSet = epsilonStates[i].split(',') # ['1','7'] (i = 0)
             for e in range(len(epsilonStateSet)):
@@ -140,40 +137,147 @@ def epsilon_closure(move,afnTranTable):
                 state = epsilonStateSet[e] #7
                 stateSet.append(state) 
                 epsilonTransitionState = find_epsilon_transitions_state(epsilonStates,afnTranTable[0],state)
+                lastAppends = 0
+                if(len(epsilonTransitionState) > 1):
+                    for ii in range(len(epsilonTransitionState)):
+                        stateSet.append(epsilonTransitionState[ii])
+                        lastAppends += 1
+                elif(len(epsilonTransitionState) == 1):
+                    stateSet.append(epsilonTransitionState[0])
+                    lastAppends += 1
+
+            s = len(stateSet)-lastAppends
+            while (s != len(stateSet)):
+                stateSetAppends = stateSet[s]
+                epsilonTransitionState = find_epsilon_transitions_state(epsilonStates,afnTranTable[0],stateSetAppends)
 
                 if(len(epsilonTransitionState) > 1):
                     for ii in range(len(epsilonTransitionState)):
                         stateSet.append(epsilonTransitionState[ii])
                 elif(len(epsilonTransitionState) == 1):
                     stateSet.append(epsilonTransitionState[0])
+                s += 1
 
-            # order states
+            # delete duplicate values
             stateSet = list(dict.fromkeys(stateSet))
-            stateSet.sort(key = int) 
+            #sort ascendent
+            stateSet.sort(key = int)
+
         #Ej. epsilonState = '1'
         elif(len(epsilonStates[i].split(',')) == 1 and epsilonStates[i] != 'vacio'):
             epsilonStateSet = epsilonStates[i]
             stateSet.append(epsilonStateSet)
             epsilonTransitionState = find_epsilon_transitions_state(epsilonStates,afnTranTable[0],epsilonStateSet)
-            #print(str(epsilonStates) +'; '+str(afnTranTable[0])+'; '+str(epsilonStateSet))
+
+            lastAppends = 0
             if(len(epsilonTransitionState) > 1):
                 for ii in range(len(epsilonTransitionState)):
                     stateSet.append(epsilonTransitionState[ii])
+                    lastAppends += 1
             elif(len(epsilonTransitionState) == 1):
                 stateSet.append(epsilonTransitionState[0])
+                lastAppends = 1
 
-            #break
+            s = len(stateSet)-lastAppends
+            while (s != len(stateSet)):
+                stateSetAppends = stateSet[s]
+                epsilonTransitionState = find_epsilon_transitions_state(epsilonStates,afnTranTable[0],stateSetAppends)
+
+                if(len(epsilonTransitionState) > 1):
+                    for ii in range(len(epsilonTransitionState)):
+                        stateSet.append(epsilonTransitionState[ii])
+                elif(len(epsilonTransitionState) == 1):
+                    stateSet.append(epsilonTransitionState[0])
+                s += 1
+            
+            # delete duplicate values
+            stateSet = list(dict.fromkeys(stateSet))
+            #sort ascendent
+            stateSet.sort(key = int)
 
     return stateSet
 
 
-def convertion_from_afn_to_afd():
+def convertion_from_afn_to_afd(afn_tran_table,alfabeto):
     '''
     Funcion que utiliza Dtran[A,a] = epsilon_closure(move(A,a)) para generar 
-    la tabla de transiciones del afd.
+    los estados del afn en la tabla de transiciones del afd.
+
+    Parametros:
+     - afn_tran_table: tabla de transiciones del afn
+     - alfabeto: el alfabeto de entrada
     '''
 
-    return 0
+    '''m = move()
+    A = epsilon_closure(m,afn_tran_table)
+    print('A = '+str(A))
+
+    m = move(A,0,afn_tran_table)
+    print('move(A,a) = '+str(m))
+    B = epsilon_closure(m,afn_tran_table)
+    print('B = '+str(B))
+
+    m = move(A,1,afn_tran_table)
+    print('move(A,b) = '+str(m))
+    C = epsilon_closure(m,afn_tran_table)
+    print('C = '+str(C))
+
+    m = move(B,0,afn_tran_table)
+    print('move(B,a) = '+str(m))
+    B = epsilon_closure(m,afn_tran_table)
+    print('B = '+str(B))
+
+    m = move(B,1,afn_tran_table)
+    print('move(B,b) = '+str(m))
+    D = epsilon_closure(m,afn_tran_table)
+    print('D = '+str(D))
+
+    m = move(D,1,afn_tran_table)
+    print('move(D,b) = '+str(m))
+    E = epsilon_closure(m,afn_tran_table)
+    print('E = '+str(E))'''
+
+    afn_states = []
+    m = move()
+    afn_states.append(epsilon_closure(m,afn_tran_table))
+    print('A = '+str(epsilon_closure(m,afn_tran_table)))
+
+    #afn_states_len = len(afn_states)
+    #for afnS in range(0,afn_states_len):
+    afnS = 0
+    while (afnS < len(afn_states)):
+        print('len(afn)'+str(afnS))
+        print('afn-states: '+str(afn_states))
+        appends = 0
+        for token in range(len(alfabeto)): #0, 1 ~ a, b
+            m = move(afn_states[afnS],token,afn_tran_table)
+            d_tran = epsilon_closure(m,afn_tran_table)
+
+            # revisamos si el conjunto de estados ya existe en afn_states
+            exist = False
+            for x in range(len(afn_states)):
+                if(d_tran == afn_states[x]):
+                    exist = True
+            # si no existe entonces agregaremos un nuevo set de estados
+            if(exist == False):
+                #revisamos si tiene el estado de aceptacion para detener el ciclo
+                print('d_tran last: '+str(d_tran[len(d_tran)-1])+' aceptacion: '+str(afn_tran_table[0][len(afn_tran_table[0])-1]))
+                if(str(d_tran[len(d_tran)-1]) == str(afn_tran_table[0][len(afn_tran_table[0])-1])): 
+                    print('sdfaslkdjfa')
+                    afn_states.append(d_tran)
+                    appends += 1
+                    #afnS = len(afn_states)
+                    #break
+                #sino contiene el estado de aceptacion continuar
+                else:
+                    print(str(afnS)+' = '+str(d_tran))
+                    afn_states.append(d_tran) 
+                    appends += 1
+        afnS += 1
+
+    
+
+    return afn_states
 
 
 # Main______________________________________________
@@ -198,11 +302,8 @@ def main():
     print(lala == lele)
     print(lala == lili)'''
 
-    m = move()
-    A = epsilon_closure(m,afn_tran_table)
-    print('A: '+str(A))
-    m = move(A,0,afn_tran_table)
-    print(m)
+    convertion_from_afn_to_afd(afn_tran_table,alfabeto)
+
 
 
 
