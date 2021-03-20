d366860814a8e724b80b7fd09d0e96619d457d9f
Diego Sevilla <sevilla.diego8@gmail.com>
2021-03-20 17:17:43 -0600

    feat: :sparkles: clase infix to postfix funcionando en main

--- main.py
+++ main.py
@@ -12,22 +12,37 @@
 import os
 import sys
 sys.path.append(".")
-from InfixPostfixRelated import infix_regex_to_postfix
+from InfixPostfixRelated.InfixRegexToPostfix import InfixRegexToPostfix
+
+from functions import *
 import collections
 
 
 # functions ________________________
 
 def welcome():
-    print("_______________________________________BIENVENIDO______________________________________")
-    print("Primera implementacion para la creacion de automatas a partir de expresiones regulares.")
+    print("________________________________________BIENVENIDO_________________________________________")
+    print("Primera implementacion para la construccion de automatas a partir de expresiones regulares.")
     print()
 
 def menu():
-    os.system('cls')
+    #os.system('cls')
     print("Seleccione una opcion.")
-    print("\t1. Metodo de Thompson")
-    print("\t1. Metodo Directo")
+    print("\t1. Metodo de Conversion por Thompson y Subconjuntos")
+    print("\t2. Metodo de Conversion Directa")
+    print("\t3. Salir")
+
+def userInteraction():
+    expresion = input('Ingrese una expresion regular: ')
+    expresion = expresion.replace(' ','')
+    cadena = input('Ingrese la cadena a evaluar: ')
+    cadena = expresion.replace(' ','')
+
+    obj = InfixRegexToPostfix()
+    postfix = obj.infix_to_postfix(expresion)
+
+    return postfix, cadena
+
 
 # Executions ________________________
 
@@ -37,20 +52,47 @@ while True:
 
     menu()
     opcion = input('Ingrese una opacion: ')
-    expresion = ''
-    cadena = ''
 
     if(opcion == '1'):
-        expresion = ''
         cadena = ''
-        print()
 
+        postfixRegex,cadena = userInteraction()
+        if(postfixRegex == 'ERROR_POSTFIX_)'):
+            print('\n ")" faltante en la expresion regular ingresada. Vuelva a intentar. \n')
+        else:
+            print('postfix   = '+ postfixRegex)
+            tokens = getRegExUniqueTokens(postfixRegex)
+            postfixRegex = stringToArray(postfixRegex)
+
+            print('alfabeto (tokens): '+str(tokens))
+            #print(str(postfixRegex))
+
+            '''objafn = AFNT(tokens,cadena)
+            AFN = objafn.generateAFN(postfixRegex)
+
+            objafd = AFDS(tokens,cadena,AFN)
+            AFD = objafd.generateAFDFromAFN()'''
+
+    elif(opcion == '2'):
         # Pruebas de funcionalidad
-        expresion = input('Ingrese una expresion regular: ')
-        expresion = expresion.replace(' ','')
-        cadena = input('Ingrese la cadena a evaluar: ')
-        cadena = expresion.replace(' ','')
+        postfixRegex,cadena = userInteraction()
+        if(postfixRegex == 'ERROR_POSTFIX_)'):
+            print('\n ")" faltante en la expresion regular ingresada. Vuelva a intentar. \n')
+        else:
+            print('\nExpresion postfix: '+ postfixRegex)
+            tokens = getRegExUniqueTokens(postfixRegex)
+            postfixRegex = stringToArray(postfixRegex)
 
-        postfix = infix_regex_to_postfix()
+            print(tokens)
+            print(postfixRegex)
+
+            '''objdirect = AFDD(tokens,cadena,postfixRegex)
+            AFD = objdirect.generateDirectAFD()'''
+
+    elif(opcion == '3'):
+        print('\nAdios! ')
+        break
+    else:
+        input('No se ha elejido ninguna opcion en el menu. Intentalo otravez! Presiona Enter!')
 
 
